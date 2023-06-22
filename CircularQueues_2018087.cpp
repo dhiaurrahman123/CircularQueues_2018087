fae9507b2b89e6758b464fb8e706ae0d59c8cb79
dhiaurrahman123 <dhiaurrahman.i.ft18@mail.umy.ac.id>
2023-06-22 10:38:49 +0700

    menentukan data kosong

--- CircularQueues_2018087.cpp
+++ CircularQueues_2018087.cpp
@@ -4,8 +4,13 @@
 using namespace std;
 
 class Queues {
-    int FRONT, REAR, max = 5;
-    int queue_array[5];
+    int FRONT, REAR, max = 5; //element
+    int queue_array[5]; //array
+public:
+    Queues() {
+        FRONT = -1;
+        REAR = -1;
+    }
 };
 
 int main()
