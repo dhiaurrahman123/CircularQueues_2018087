22e7e704c7b7a660f301c8da611389c5f7b4572a
dhiaurrahman123 <dhiaurrahman.i.ft18@mail.umy.ac.id>
2023-06-22 11:23:12 +0700

    membuat perintah insert

--- CircularQueues_2018087.cpp
+++ CircularQueues_2018087.cpp
@@ -101,8 +101,18 @@ int main()
             cout << "2. Implement delete operation" << endl;
             cout << "3. Display values" << endl;
             cout << "4. Exit" << endl;
-            cout << "Enter your choice (1-4)":  << endl;
+            cout << "Enter your choice (1-4): "  << endl;
             cin >> ch;
+            cout << endl;
+            
+            switch (ch)
+            {
+            case '1': {
+                q.insert();
+                break;
+                }
+            }
+            
 
         }
     }
