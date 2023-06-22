1637cb47fad5e955723145deed340b472e353b57
dhiaurrahman123 <dhiaurrahman.i.ft18@mail.umy.ac.id>
2023-06-22 11:20:01 +0700

    membuat tampilan menu

--- CircularQueues_2018087.cpp
+++ CircularQueues_2018087.cpp
@@ -91,6 +91,21 @@ public:
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Queues q;
+    char ch;
+    
+    while (true) {
+        try {
+            cout << "Menu" << endl;
+            cout << "1. Implement insert operation" << endl;
+            cout << "2. Implement delete operation" << endl;
+            cout << "3. Display values" << endl;
+            cout << "4. Exit" << endl;
+            cout << "Enter your choice (1-4)":  << endl;
+            cin >> ch;
+
+        }
+    }
+    
 }
 
